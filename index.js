c0c02c09a5c187cf0bf3913b775ad4239215d648
dewerner <werner.huysmans@gmail.com>
2020-05-25 23:07:19 +0200

    ok

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+let port = process.env.PORT || 3000
 const axios = require('axios')
-const baseURL = 'https://canvas.kdg.be/api/v1/' 
+let school = ''
 let quizID = 0
 let courseID = 0
 let token = '' 
@@ -15,11 +15,34 @@ app.get('/', ( req, res ) => {
 	res.sendFile( path.join( __dirname + '/index.html' ) )
 } )
 
+const createAnswerRow = ( doc, answer, type ) => {
+	doc
+		.fontSize( 9 )
+		.text( type === 'fill_in_multiple_blanks_question' ?
+			`${ answer.text } (${ answer.blank_id })\n` :
+			`${ answer.text }\n`, {
+				indent: 10
+			}	
+		)
+}
+
+const generateQuestionRow = ( doc, question ) => {
+	doc
+		.fontSize( 12 )
+		.text( `${ question.question_text }\n\n` )
+}
+
+const generateSpace = ( doc ) => {
+	doc
+		.text( `\n\n` )
+}
+
 app.get('/test', async ( req, res ) => {
 	quizID = req.query.assignment
 	courseID = req.query.course
 	token = `Bearer ${ req.query.token }`
-	let quizURL = `${ baseURL }courses/${ courseID }/quizzes/${ quizID }/questions`
+	school = req.query.school
+	let quizURL = `${ school }/api/v1/courses/${ courseID }/quizzes/${ quizID }/questions`
 	let result = []
 	try {
 		let keepGoing = true
@@ -45,20 +68,22 @@ app.get('/test', async ( req, res ) => {
 		}
 		console.log( result )
 		const doc = new PDFDocument
-		let str = 'Questions for this Quiz\n'
+		let str = 'Questions for this Quiz\n\n'
 		result.map( ( questionBlock ) => {
 			let item = {}
-			str += `${ questionBlock.question_text }\n\n`
+			// str += `${ questionBlock.question_text }\n\n`
+			generateQuestionRow( doc, questionBlock )
 			questionBlock.answers.map( ( answer ) => {
-				if ( questionBlock.question_type === 'fill_in_multiple_blanks_question' ) {
-					str += `${ answer.text } (${ answer.blank_id })\n`
-				} else {
-					str += `${ answer.text }\n`
-				}
+				createAnswerRow( doc, answer, questionBlock.question_type )
+				// if ( questionBlock.question_type === 'fill_in_multiple_blanks_question' ) {
+				// 	str += `${ answer.text } (${ answer.blank_id })\n`
+				// } else {
+				// 	str += `${ answer.text }\n`
+				// }
 			} )
-			str += `\n\n`
+			generateSpace( doc )
 		} )
-		doc.text( str )
+		// doc.text( str )
 		doc.pipe( res )
 		doc.end()
 	} catch ( err ) {
